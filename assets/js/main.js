3e171f50cbc80b8597eee7ebf5d32ef00bfee20a
agnescameron <agnesfcameron@gmail.com>
2019-07-17 01:44:54 +0300

    recipes populate grid, now toggles letter

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
-function closeLetter() {
+function toggleLetter() {
 	console.log('closing letter')
-	$('.letterBox').remove()
+	$('.letterBox').toggle()
 }
 
 //draw grid
@@ -24,8 +24,27 @@ function drawGrid(){
     ctx.font = "15px Helvetica";
     ctx.fillText("fast", 675, 340);
     ctx.fillText("slow", 20, 340);
-    ctx.fillText("salad", 330, 695);
-    ctx.fillText("fried", 330, 15);
+    ctx.fillText("fried", 330, 695);
+    ctx.fillText("salad", 330, 15);
+
+    //each grid cube is 35 by 35
+
+    for(var i=0; i<recipes.length; i++){
+
+      //don't do it w canvas because it makes interaction hard
+      // console.log(recipes[i].name)
+      // ctx.fillText(recipes[i].name.replace(/-/g, ' '), 35*(recipes[i].speed+10), (700-35*(recipes[i].salad+10)))
+
+      $('<div />', {
+        id: recipes[i].name,
+        class: 'recipeName',
+        click: function(){ console.log(this.id) }, 
+      })
+      .html(recipes[i].name.replace(/-/g, ' '))
+      .css({left: 35*(recipes[i].speed+10), top: (690-35*(recipes[i].salad+10)) })
+      .appendTo($('#canvasCover'))
+
+     }
 }
 
 
---
+++ assets/js/recipes.js
@@ -0,0 +1,134 @@
+var recipes = [
+  //speed, salad from -10 to +10, where -10 is slow, fried, and +10 is fast, salad
+  {
+    'name': 'beef-peppers-noodles',
+    'speed': 4,
+    'salad': -4,
+  },
+
+  {
+    'name': 'braised-aubergine',
+    'speed': 4,
+    'salad': 0.5,
+  },
+
+  {
+    'name': 'cabbage-omelette',
+    'speed': 6,
+    'salad': 1,
+  },
+
+  {
+    'name': 'cold-soba',
+    'speed': 4,
+    'salad': 4,
+  },
+  
+  {
+    'name': 'congee',
+    'speed': -2,
+    'salad': 4,
+  },
+
+  {
+    'name': 'cucumber-salad',
+    'speed': 9,
+    'salad': 9,
+  },
+
+  {
+    'name': 'dumplings',
+    'speed': -4,
+    'salad': -4,
+  },
+
+  {
+    'name': 'egg-and-tomato',
+    'speed': 4,
+    'salad': 2,
+  },      
+
+  {
+    'name': 'egg-fried-rice',
+    'speed': 6,
+    'salad': -3,
+  },      
+
+  {
+    'name': 'fried-enokis',
+    'speed': 9,
+    'salad': 5,
+  },      
+
+  {
+    'name': 'fried-greens',
+    'speed': 8.5,
+    'salad': 8,
+  },      
+
+  {
+    'name': 'green-beans',
+    'speed': 5,
+    'salad': 7,
+  },      
+
+  {
+    'name': 'grilled-mackerel',
+    'speed': 5,
+    'salad': 5,
+  },      
+
+  {
+    'name': 'hot-fish-soup',
+    'speed': -1,
+    'salad': 3,
+  },      
+
+  {
+    'name': 'komatsuna-nibitashi',
+    'speed': 7,
+    'salad': 8.5,
+  },      
+
+  {
+    'name': 'korean-hot-soup',
+    'speed': 4,
+    'salad': -1,
+  },      
+
+  {
+    'name': 'mapo-tofu',
+    'speed': 4,
+    'salad': -2,
+  },      
+
+  {
+    'name': 'red-braised-fish',
+    'speed': 2,
+    'salad': -5,
+  },      
+
+  {
+    'name': 'salt-cod-stew',
+    'speed': -3,
+    'salad': 1,
+  },      
+
+  {
+    'name': 'soba-soup',
+    'speed': 6,
+    'salad': 6,
+  },      
+
+  {
+    'name': 'steamed-fish',
+    'speed': 1,
+    'salad': 6,
+  },      
+
+  {
+    'name': 'tasty-miso',
+    'speed': 9,
+    'salad': 3,
+  },   
+]
