13ce606a829e88ffb20335ab6991dbc87d9193a2
agnescameron <agnesfcameron@gmail.com>
2019-07-17 00:45:19 +0300

    label axes

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,14 +13,19 @@ function drawGrid(){
 
 	ctx.beginPath();
 
-    ctx.moveTo(0, 350);
+    ctx.moveTo(20, 350);
     ctx.lineTo(700, 350);
     ctx.stroke();
 
-    ctx.moveTo(350, 0);
-    ctx.lineTo(350, 700);
+    ctx.moveTo(350, 20);
+    ctx.lineTo(350, 680);
     ctx.stroke();
 
+    ctx.font = "15px Helvetica";
+    ctx.fillText("fast", 675, 340);
+    ctx.fillText("slow", 20, 340);
+    ctx.fillText("salad", 330, 695);
+    ctx.fillText("fried", 330, 15);
 }
 
 
