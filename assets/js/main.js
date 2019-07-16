70412a81b6a425e7cee130efee41ee53f4abdcbb
agnescameron <agnesfcameron@gmail.com>
2019-07-16 15:12:40 +0300

    draws basic grid

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,26 @@ function closeLetter() {
 	console.log('closing letter')
 	$('.letterBox').remove()
 }
+
+//draw grid
+function drawGrid(){
+
+	var canvas = document.getElementById("canvas");
+  	var ctx = document.getElementById('canvas').getContext('2d');
+
+  	ctx.lineWidth = 3;
+
+	ctx.beginPath();
+
+    ctx.moveTo(0, 350);
+    ctx.lineTo(700, 350);
+    ctx.stroke();
+
+    ctx.moveTo(350, 0);
+    ctx.lineTo(350, 700);
+    ctx.stroke();
+
+}
+
+
+drawGrid();
