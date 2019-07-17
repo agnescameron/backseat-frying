34f4d7aa1e18bf5c2a04472729ba45036d8ab1ac
agnescameron <agnesfcameron@gmail.com>
2019-07-17 15:42:28 +0300

    added a whole load of recipes, and, more importantly, the oracle

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,145 @@
-function toggleLetter() {
+function closeLetter() {
 	console.log('closing letter')
-	$('.letterBox').toggle()
+	$('.letterBox').hide()
 }
+
+function openLetter() {
+  console.log('opening letter')
+  $('.letterBox').show()
+}
+
+
+function getEntries(array, type, val) {
+  return array.filter(function (el) {
+    return el[type] === val;
+  });
+}
+
+//scrubs entry from array
+function remove(array, entry) {
+  var index = array.indexOf(entry);
+    if (index > -1) {
+      array.splice(index, 1);
+    }
+}
+
+//the AI
+function generateMeal () {
+  var people = Math.floor(Math.random()*5)+1;
+  var points = [];
+  var option;
+  var mains = getEntries(recipes, "classification", "main");
+  var veg = getEntries(recipes, "classification", `veg`);
+  var sides = getEntries(recipes, "classification", `side`);
+  var soups = getEntries(recipes, "classification", `soup`);  
+  var breakfastcarbs = getEntries(recipes, "classification", `breakfast-carb`);
+  var smallsoups = getEntries(recipes, "classification", `small-soup`);
+
+  if(people === 1){
+    var choices = sides.concat(soups);
+    var choice = choices[Math.floor(Math.random()*choices.length)];
+    points.push(choice);
+  }
+
+  if(people === 2){
+    //line between dishes
+    //2 sides, soup+veg, soup+side, side+veg, main+veg
+    var choices = sides.concat(soups).concat(veg);
+    var choice1 = choices[Math.floor(Math.random()*choices.length)];
+    points.push(choice1); 
+    remove(choices, choice1)   
+    var choice2 = choices[Math.floor(Math.random()*choices.length)];
+    points.push(choice2); 
+  }
+
+  if(people === 3){
+    //line between dishes
+    //main and 2 of veg and side, or soup and 2 of veg and side
+    option = Math.floor(Math.random()*2);
+    console.log('option is', option)
+
+    if(option === 0){
+      //pick a main, then randomly 2 sides or veg
+      var main = mains[Math.floor(Math.random()*mains.length)];
+      points.push(main)      
+    }
+
+    else{
+      //pick a soup, then randomly 2 sides or veg
+      var soup = soups[Math.floor(Math.random()*soups.length)];
+      points.push(soup)         
+    }
+
+    var extras = veg.concat(sides)
+    var extra1 = extras[Math.floor(Math.random()*extras.length)];
+    points.push(extra1)
+    remove(extras, extra1);
+    var extra2 = extras[Math.floor(Math.random()*extras.length)];
+    points.push(extra2)
+
+  }
+
+  if(people === 4){
+    //line between dishes
+    //any 4 dishes excluding breakfast and containing main
+    //or breakfast, with 3 veg
+    var main = mains[Math.floor(Math.random()*mains.length)];
+    points.push(main)
+
+    var extras = veg.concat(sides).concat(soup)
+    var extra1 = extras[Math.floor(Math.random()*extras.length)];
+    points.push(extra1)
+    remove(extras, extra1);
+    var extra2 = extras[Math.floor(Math.random()*extras.length)];
+    points.push(extra2)
+    remove(extras, extra2);
+    var extra3 = extras[Math.floor(Math.random()*extras.length)];
+    points.push(extra3)
+
+  }
+
+  if(people === 5){
+    //line between dishes
+    //any 4 dishes excluding breakfast and containing main
+    //or breakfast, with 3 veg
+    var main1 = mains[Math.floor(Math.random()*mains.length)];
+    points.push(main1)
+    remove(mains, main1);
+    var main2 = mains[Math.floor(Math.random()*mains.length)];
+    points.push(main2)
+
+    var extras = veg.concat(sides).concat(soup)
+    var extra1 = extras[Math.floor(Math.random()*extras.length)];
+    points.push(extra1)
+    remove(extras, extra1);
+    var extra2 = extras[Math.floor(Math.random()*extras.length)];
+    points.push(extra2)
+    remove(extras, extra2);
+    var extra3 = extras[Math.floor(Math.random()*extras.length)];
+    points.push(extra3)
+
+  }
+
+
+  descriptors = ["stunning", "delicious", "charming", "delightful", "romantic", "filling", "nutritious", "generous", "luxuriant", "healthy", "simple"];
+  descriptor = descriptors[Math.floor(Math.random()*descriptors.length)];
+
+  var recipeList = '';
+  console.log(points)
+
+  for(var i=(points.length-1); i>=0; i--){
+    console.log(recipeList, i)
+    if (i===0) recipeList = recipeList + points[i].name.replace(/-/g, ' ');
+    else if (i===1) recipeList = recipeList + points[i].name.replace(/-/g, ' ') + ' and ';
+    else recipeList = recipeList + `${points[i].name.replace(/-/g, ' ')}, `
+  }
+
+  var oracleText = `The oracle recommends a ${descriptor} meal for ${people} of ${recipeList}` 
+  $('#letterContents').html(oracleText)
+  $('.letterBox').show()
+}
+
+function drawLine(start, finish){}
 
 //draw grid
 function drawGrid(){
--- assets/js/recipes.js
+++ assets/js/recipes.js
@@ -4,131 +4,160 @@ var recipes = [
     'name': 'beef-peppers-noodles',
     'speed': 4,
     'salad': -4,
+    'classification': 'main'
   },
 
   {
     'name': 'braised-aubergine',
     'speed': 4,
     'salad': 0.5,
+    'classification': 'side'
   },
 
   {
     'name': 'cabbage-omelette',
     'speed': 6,
     'salad': 1,
+    'classification': 'side'
   },
 
   {
     'name': 'cold-soba',
     'speed': 4,
     'salad': 4,
+    'classification': 'breakfast-carb'
   },
   
   {
     'name': 'congee',
     'speed': -2,
     'salad': 4,
+    'classification':'breakfast-carb'
   },
 
   {
     'name': 'cucumber-salad',
     'speed': 9,
     'salad': 9,
+    'classification': 'veg'
   },
 
   {
     'name': 'dumplings',
     'speed': -4,
     'salad': -4,
+    'classification': 'main'
   },
 
   {
     'name': 'egg-and-tomato',
     'speed': 4,
     'salad': 2,
+    'classification': 'side'
   },      
 
   {
     'name': 'egg-fried-rice',
     'speed': 6,
     'salad': -3,
+    'classification': 'main'
   },      
 
   {
     'name': 'fried-enokis',
     'speed': 9,
     'salad': 5,
+    'classification': 'veg'
   },      
 
   {
     'name': 'fried-greens',
     'speed': 8.5,
     'salad': 8,
+    'classification': 'veg'
   },      
 
   {
     'name': 'green-beans',
     'speed': 5,
     'salad': 7,
+    'classification': 'side'
   },      
 
   {
     'name': 'grilled-mackerel',
     'speed': 5,
     'salad': 5,
+    'classification': 'main'
   },      
 
   {
     'name': 'hot-fish-soup',
     'speed': -1,
     'salad': 3,
+    'classification': 'soup'
   },      
 
   {
     'name': 'komatsuna-nibitashi',
     'speed': 7,
     'salad': 8.5,
+    'classification': 'veg'
   },      
 
   {
     'name': 'korean-hot-soup',
     'speed': 4,
     'salad': -1,
+    'classification': 'soup'
   },      
 
   {
     'name': 'mapo-tofu',
     'speed': 4,
     'salad': -2,
+    'classification': 'side'
   },      
+
+  {
+    'name': 'miso-aubergine',
+    'speed': 0,
+    'salad': 2,
+    'classification': 'side'
+  },
 
   {
     'name': 'red-braised-fish',
     'speed': 2,
     'salad': -5,
+    'classification': 'main'
   },      
 
   {
     'name': 'salt-cod-stew',
     'speed': -3,
     'salad': 1,
+    'classification': 'main'
   },      
 
   {
     'name': 'soba-soup',
     'speed': 6,
     'salad': 6,
+    'classification': 'soup'
   },      
 
   {
     'name': 'steamed-fish',
     'speed': 1,
     'salad': 6,
+    'classification': 'main'
   },      
 
   {
     'name': 'tasty-miso',
     'speed': 9,
     'salad': 3,
+    'classification': 'small-soup'
   },   
 ]
