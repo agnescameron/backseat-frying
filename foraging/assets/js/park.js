b38c759c204124855bbb336e54da842c3fc7859c
agnescameron <agnesfcameron@gmail.com>
2020-04-23 13:07:09 -0400

    foraging

---
+++ foraging/assets/js/park.js
@@ -0,0 +1,78 @@
+import { plants, patches } from './plants.js';
+
+// constants
+const numPlants = 100;
+
+//evil global variable :o 
+let bag = [];
+
+
+//this function calculates the number
+//of plants of a specified type in the bag
+
+//assignment 3: replace this with
+//a single-line arrow function
+function countPlants(plantName) {
+	let total = 0;
+
+	for(let i=0; i<bag.length; i++){
+		if(bag[i].name === plantName) total+=1
+	}
+
+	return total;
+}
+
+//this function 'picks' a plant from the 
+//field and appends it to the bag
+function pickPlant(plantDiv) {
+	const plant = plantDiv.data('plant')
+	bag.push(plant)
+
+	//append the item to the bag div
+	$('#bag-contents').append($('<div/>', {
+			'class': 'bag-item'
+		})
+		.html(plant.name))
+
+	const total = countPlants(plant.name)
+	console.log(`there are now ${total} ${plant.name} in the bag`)
+}
+
+//this function randomly generates plants on the
+//main 'field' at the start of the simulation
+function growPlants() {
+	patches.forEach( (patch) => {
+		const plant = plants.filter((plant) => plant.name === patch.plant)[0]
+
+		let plantDiv = $('<div/>', {
+			'class': `plant ${plant.name}`
+		})
+		//this is the jquery .data() attribute
+		.data('plant', plant)
+		.html(plant.symbol)
+		.css({'left': patch.locationX*100 + 'vw', 'top': patch.locationY*100 + 'vw', 'color': plant.color})
+		.click(function() {
+			pickPlant($(this));
+		})
+		.hover(function() {
+			$(this).find('span').show();
+		}, function() {
+			$(this).find('span').hide();
+		})
+		$('#map').append(plantDiv)
+
+		plantDiv.append(`<span class="plant-name">${plant.name}</span>`)
+	})
+}
+
+//this code runs once the document has loaded
+$(document).ready(function() {
+	growPlants();
+
+	//event listeners
+	$('#map').click(function(event){
+		console.log("locationX: " + event.pageX/$(window).width() + `, \nlocationY:` + event.pageY/$(window).width() + ",")
+	})
+	$('#bag').click(() => $('#bag-contents').show())
+	$('#bag-contents').click(() => $('#bag-contents').hide())
+})
---
+++ foraging/assets/js/plants.js
@@ -0,0 +1,173 @@
+export const plants = [
+
+	{
+		'name': 'mugwort',
+		'symbol': 'ӂ',
+		'color': 'mediumseagreen',
+		'recipes': ['mugwort tea']
+	},
+
+	{
+		'name': 'japanese knotweed',
+		'symbol': 'Ѫ',
+		'color': 'greenyellow',
+		'recipes': ['knotweed crumble']
+	},
+
+	{
+		'name': 'chickweed',
+		'symbol': 'Ѫ',
+		'color': 'limegreen',
+		'recipes': ['chickweed pakora']
+	},
+
+	{
+		'name': 'dock',
+		'symbol': 'Գ',
+		'color': 'forestgreen',
+		'recipes': ['hindbeh']
+	},
+
+	{
+		'name': 'garlic mustard',
+		'symbol': 'Գ',
+		'color': 'greenyellow',
+		'recipes': ['hindbeh']
+	},
+
+	{
+		'name': 'wild onion',
+		'symbol': '҂',
+		'color': 'seagreen',
+		'recipes': ['stir fried eggs and wild onion']
+	},
+
+	{
+		'name': 'dandelion greens',
+		'symbol': '෴',
+		'color': 'olivedrab',
+		'recipes': ['hindbeh']
+	},
+
+]
+
+export const patches = [
+	{
+		plant: "wild onion",
+		locationX: 0.6648983200707339, 
+		locationY:0.1246684350132626,
+		abundance: "high",
+	},
+
+	{
+		plant: "wild onion",
+		locationX: 0.34129089301503096, 
+		locationY:0.6030061892130858,
+		abundance: "low",
+	},
+
+	{
+		plant: "chickweed",
+		locationX: 0.7603890362511052, 
+		locationY:0.3660477453580902,
+		abundance: "low",
+	},
+
+	{
+		plant: "mugwort",
+		locationX: 0.6534040671971706, 
+		locationY:0.07161803713527852,
+		abundance: "high",
+	},
+
+	{
+		plant: "mugwort",
+		locationX: 0.3333333333333333, 
+		locationY:0.6463306808134395,
+		abundance: "high",
+	},
+
+	{
+		plant: "garlic mustard",
+		locationX: 0.6480990274093722, 
+		locationY:0.5464190981432361,
+		abundance: "high",
+	},
+
+	{
+		plant: "garlic mustard",
+		locationX: 0.6454465075154731, 
+		locationY:0.5632183908045977,
+		abundance: "high",
+	},
+
+	{
+		plant: "garlic mustard",
+		locationX: 0.6816976127320955, 
+		locationY:0.4562334217506631,
+		abundance: "high",
+	},
+
+	{
+		plant: "garlic mustard",
+		locationX: 0.7963206307490145, 
+		locationY:0.5085413929040736,
+		abundance: "medium",
+	},
+
+	{
+		plant: "dock",
+		locationX: 0.1246684350132626, 
+		locationY:0.7161803713527851,
+		abundance: "medium",
+	},
+
+	{
+		plant: "dock",
+		locationX: 0.5853227232537578, 
+		locationY:0.7824933687002652,
+		abundance: "high",
+	},
+
+	{
+		plant: "dandelion greens",
+		locationX: 0.797634691195795, 
+		locationY:0.519053876478318,
+		abundance: "medium",
+	},
+
+	{
+		plant: "dandelion greens",
+		locationX: 0.7936925098554534, 
+		locationY:0.5085413929040736,
+		abundance: "medium",
+	},
+
+	{
+		plant: "dandelion greens",
+		locationX: 0.3061760840998686, 
+		locationY:0.6386333771353482,
+		abundance: "medium",
+	},
+
+	{
+		plant: "dandelion greens",
+		locationX: 0.1287779237844941, 
+		locationY:0.6491458607095927,
+		abundance: "medium",
+	},
+
+	{
+		plant: "japanese knotweed",
+		locationX: 0.7187910643889619, 
+		locationY:0.47174770039421815,
+		abundance: "high",
+	},
+
+	{
+		plant: "japanese knotweed",
+		locationX: 0.6189224704336399, 
+		locationY:0.5597897503285151,
+		abundance: "high",
+	}
+]
